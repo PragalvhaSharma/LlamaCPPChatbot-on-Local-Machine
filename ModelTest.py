b434903e3fe802f75464346240547c32e49e7763
Pragalvha Sharma <140682795+PragalvhaSharma@users.noreply.github.com>
2024-01-23 11:53:19 -0500

    Update ModelTest.py

--- ModelTest.py
+++ ModelTest.py
@@ -1,32 +1,17 @@
-from llama_cpp import Llama 
-import timeit
+from llama_cpp import Llama
 
-# Load Llama 2 model
-llm = Llama(model_path="Model Path",
-            n_ctx=512,
-            n_batch=128)
+llm = Llama(
+  model_path="darevox-7b.Q5_K_S.gguf",  # Download the model file first
+  n_ctx=32768,  # The max sequence length to use - note that longer sequence lengths require much more resources
+  n_threads=8,            # The number of CPU threads to use, tailor to your system and the resulting performance      # The number of layers to offload to GPU, if you have GPU acceleration available
+)
 
-# Start timer
-start = timeit.default_timer()
+prompt = "QUery"
+output = llm(
+    f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{prompt}\n\n### Response: ", # Prompt
+    max_tokens=512, # Generate up to 32 tokens
+    stop=["</s>"], # Stop generating just before the model would generate a new question
+    echo=True # Echo the prompt back in the output
+) # Generate a completion, can also call create_completion
+print(output["choices"][0]["text"])
 
-# Generate LLM response
-prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-
-output = llm(prompt,
-             max_tokens=-1,
-             echo=False,
-             temperature=0.1,
-             top_p=0.9)
-
-# Stop timer
-stop = timeit.default_timer()
-duration = stop - start
-print("Time: ", duration, '\n\n')
-
-# Display generated text
-print(output['choices'][0]['text'])
-
-# Write to file
-with open("response.txt", "a") as f:
-  f.write(f"Time: {duration}")
-  f.write(output['choices'][0]['text'])
